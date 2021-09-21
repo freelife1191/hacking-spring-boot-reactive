dd7063c594635f95fa260e27fa49de238846e52b
freelife1191 <freejava1191@gmail.com>
2021-09-21 19:12:56 +0900

    9장 스프링 부트 애플리케이션 보안 - 사용자 컨텍스트 접근

--- src/main/java/com/greglturnquist/hackingspringboot/reactive/HomeController.java
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/HomeController.java
@@ -20,22 +20,44 @@ public class HomeController {
         this.inventoryService = inventoryService;
     }
 
+    /**
+     * 사용자별 장바구니 작성
+     * @param auth
+     * @return
+     */
     @GetMapping
-    Mono<Rendering> home(Authentication auth) { // <1>
-        return Mono.just(Rendering.view("home.html") //
-                .modelAttribute("items", this.inventoryService.getInventory()) //
-                .modelAttribute("cart", this.inventoryService.getCart(cartName(auth)) // <2>
-                        .defaultIfEmpty(new Cart(cartName(auth)))) //
-                .modelAttribute("auth", auth) // <3>
+    // Authentication을 home() 메소드의 인자로 추가하면 스프링 시큐리티가 구독자 컨텍스트(subscriber context)에서
+    // Authentication 정보를 추출해서 인자로 주입해준다
+    Mono<Rendering> home(Authentication auth) {
+        return Mono.just(Rendering.view("home.html")
+                .modelAttribute("items", this.inventoryService.getInventory())
+                // auth를 인자로 전달해서 cartName() 메소드를 호출하면 장바구니 id를 반환한다
+                .modelAttribute("cart", this.inventoryService.getCart(cartName(auth))
+                        .defaultIfEmpty(new Cart(cartName(auth))))
+                // Authentication 객체를 템플릿에 모델로 제공해주면, 템플릿이 웹 페이지의 컨텍스트에
+                // 모델 데이터를 담아서 사용할 수 있게 된다
+                .modelAttribute("auth", auth)
                 .build());
     }
 
+    /**
+     * Item 추가
+     * @param auth
+     * @param id
+     * @return
+     */
     @PostMapping("/add/{id}")
     Mono<String> addToCart(Authentication auth, @PathVariable String id) {
         return this.inventoryService.addItemToCart(cartName(auth), id) //
                 .thenReturn("redirect:/");
     }
 
+    /**
+     * Item 삭제
+     * @param auth
+     * @param id
+     * @return
+     */
     @DeleteMapping("/remove/{id}")
     Mono<String> removeFromCart(Authentication auth, @PathVariable String id) {
         return this.inventoryService.removeOneFromCart(cartName(auth), id) //
